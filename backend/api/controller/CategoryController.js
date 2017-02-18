e5d0a1fc76e77a7f397182e90dcc081834da5504
devgitcode <devgitcode@gmail.com>
2017-02-18 19:57:00 +0700

    [Hao] Thêm function cho Category tại Backend

--- backend/api/controller/CategoryController.js
+++ backend/api/controller/CategoryController.js
@@ -1,6 +1,7 @@
 var connection = require('../database/DBConnection');
 
 module.exports = function(app){
+    
     app.get('/categories', function(req, res){
          connection.query("SELECT * FROM Categories", function(error, result){
             if(error) throw error;
@@ -8,6 +9,7 @@ module.exports = function(app){
             res.send(result);
         });
     });
+
     app.get('/category/:id', function(req, res){
          connection.query(`SELECT * FROM Categories WHERE id = ${req.params.id}`, function(error, result){
             if(error) throw error;
@@ -15,4 +17,19 @@ module.exports = function(app){
             res.json(result);
         });
     });
+
+    app.post('/category',function(req, res){
+        connection.query(`INSERT INTO Categories VALUES(null, '${req.body.category_name}', ${req.body.order})`, function(error, result){
+            if(error) throw error;
+            res.send(result);
+        });
+    });
+
+    app.put('/category', function(req, res){
+        connection.query(`UPDATE Categories SET category_name = '${req.body.category_name}', \`order\`= ${req.body.order} WHERE id = ${req.body.id}`, function(error, result){
+            if(error) throw error;
+            res.send(result);
+        });
+    });
+
 }
